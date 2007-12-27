ab13ec58d6ff07ce12097e54f4dd45e0467f6cdb
courtenay <court3nay@gmail.com>
2007-12-26 17:49:05 -0800

    make all tests pass; add a test for first-user login

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,7 +2,15 @@ require File.dirname(__FILE__) + '/../spec_helper'
 
 describe UsersController do
   define_models
-
+  define_models do
+    model User do
+      #stub :admin,     :login => 'admin-user',     :email => 'admin-user@example.com', :remember_token => 'blah'
+      stub :pending,   :login => 'pending-user',   :email => 'pending-user@example.com',   :state => 'pending', :activated_at => nil, :remember_token => 'asdf',
+        :activation_code => 'foo-bar-22',
+        :salt => '7e3041ebc2fc05a40c60028e2c4901a81035d3cd', :crypted_password => '00742970dc9e6319f8019fd54864d3ea740f04b1'
+    end
+  end
+  
   it 'allows signup' do
     lambda do
       create_user
@@ -58,6 +66,14 @@ describe UsersController do
   it 'does not activate user with blank key' do
     get :activate, :activation_code => ''
     flash[:notice].should be_nil
+  end
+  
+  it 'logs in the first user' do
+    User.delete_all
+    create_user
+    user = User.find_by_login('quire')
+    assigns[:current_user].should == user
+    assigns[:current_user].should be_admin
   end
   
   def create_user(options = {})
