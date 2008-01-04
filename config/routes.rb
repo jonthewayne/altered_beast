f4c2fa40221d3c9c770efac87f14d728fb2e4fd4
rick <technoweenie@gmail.com>
2008-01-03 16:22:37 -0800

    removed some TODOs on forums/index

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,24 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :sites
 
-  map.resources :forums
+  map.resources :forums, :has_many => :posts do |forum|
+    forum.resources :topics do |topic|
+      topic.resources :posts
+      topic.resource :monitorship
+    end
+    forum.resources :posts
+  end
+  
+  map.resources :posts, :collection => {:search => :get}
+
   map.resources :users, :member => { :suspend   => :put,
                                      :unsuspend => :put,
                                      :purge     => :delete }
+
   map.activate '/activate/:activation_code', :controller => 'users',    :action => 'activate', :activation_code => nil
   map.signup   '/signup',                    :controller => 'users',    :action => 'new'
   map.login    '/login',                     :controller => 'sessions', :action => 'new'
   map.logout   '/logout',                    :controller => 'sessions', :action => 'destroy'
   map.resource  :session
-  map.resources :sites
   map.root :controller => 'forums', :action => 'index'
 end
